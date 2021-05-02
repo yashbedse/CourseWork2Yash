3d46de07972b5fbb5a8d43ed43bad24774664c18
yashbedse <19127385@brookes.ac.uk>
2021-05-02 21:11:02 +0300

    File Directory Changes
    Changing of file directories to allow for the execution of bash scripts (production and development) from the root folder-commands.
    
    Co-Authored-By: Rohit19044580 <60389479+Rohit19044580@users.noreply.github.com>
    Co-Authored-By: nagendrakonakalla220197 <82903017+nagendrakonakalla220197@users.noreply.github.com>
    Co-Authored-By: aminindbd <82466049+aminindbd@users.noreply.github.com>

--- commands/development/start-composer.sh
+++ commands/development/start-composer.sh
@@ -4,18 +4,18 @@
 # Create the .env file if it does not exist.
 # ----------------------------------------------------------------------
 
-if [[ ! -f "/app/.env" ]] && [[ -f "/app/.env.example" ]];
+if [[ ! -f "../../app/.env" ]] && [[ -f "../../app/.env.example" ]];
 then
-cp /app/.env.example /app/.env
+cp ../../app/.env.example /app/.env
 fi
 
 # ----------------------------------------------------------------------
 # Run Composer
 # ----------------------------------------------------------------------
 
-if [[ ! -d "/app/vendor" ]];
+if [[ ! -d "../../app/vendor" ]];
 then
-cd /app
+cd ../../app
 composer install
 composer dump-autoload -o
 fi
--- commands/production/start-composer.sh
+++ commands/production/start-composer.sh
@@ -4,9 +4,9 @@
 # Create the .env file if it does not exist.
 # ----------------------------------------------------------------------
 
-if [[ ! -f "/app/.env" ]] && [[ -f "/app/.env.example" ]];
+if [[ ! -f "../../app/.env" ]] && [[ -f "../../app/.env.example" ]];
 then
-cp /app/.env.example /app/.env
+cp ../../app/.env.example ../../app/.env
 fi
 
 # ----------------------------------------------------------------------
@@ -15,7 +15,7 @@ fi
 
 if [[ ! -d "/app/vendor" ]];
 then
-cd /app
+cd ../../app
 composer install
 composer dump-autoload -o
 fi
